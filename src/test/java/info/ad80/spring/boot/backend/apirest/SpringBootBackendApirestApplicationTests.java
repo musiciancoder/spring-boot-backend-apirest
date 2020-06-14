de880d5a6406453e5f1588f7608375d818bf1afc
musiciancoder <andresguitarra1980@gmail.com>
2020-06-14 13:02:18 -0400

    28. creando proyecto backend API REST

---
+++ src/test/java/info/ad80/spring/boot/backend/apirest/SpringBootBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package info.ad80.spring.boot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

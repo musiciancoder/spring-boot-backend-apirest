7f8f0a94346895549259cd6073fb497cf3b49aae
musiciancoder <andresguitarra1980@gmail.com>
2020-06-15 12:56:36 -0400

    41. Escribiendo los métodos show y create en el Controlador Backend API Rest

--- src/main/java/info/ad80/spring/boot/backend/apirest/services/IClienteService.java
+++ src/main/java/info/ad80/spring/boot/backend/apirest/services/IClienteService.java
@@ -8,6 +8,12 @@ public interface IClienteService {
 
 	public List<Cliente> findAll();
 	
+	public Cliente findById(Long id);
+	
+	public Cliente save (Cliente cliente);
+	
+	public void delete (Long id);
+	
 	
 		
 	

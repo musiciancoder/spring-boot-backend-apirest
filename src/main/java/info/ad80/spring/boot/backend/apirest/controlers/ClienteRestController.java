4cec36ebba4d08bd5a979787fa6e28338c5a5018
musiciancoder <andresguitarra1980@gmail.com>
2020-06-17 16:46:46 -0400

    56. Manejo de error en el Backend en método handler show (obtener por id)

--- src/main/java/info/ad80/spring/boot/backend/apirest/controlers/ClienteRestController.java
+++ src/main/java/info/ad80/spring/boot/backend/apirest/controlers/ClienteRestController.java
@@ -1,10 +1,13 @@
 package info.ad80.spring.boot.backend.apirest.controlers;
 
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,56 +22,75 @@ import org.springframework.web.bind.annotation.RestController;
 import info.ad80.spring.boot.backend.apirest.models.entity.Cliente;
 import info.ad80.spring.boot.backend.apirest.services.IClienteService;
 
-
-@CrossOrigin(origins= {"http://localhost:4200"}) //CORS, para conectar con Angular
+@CrossOrigin(origins = { "http://localhost:4200" }) // CORS, para conectar con Angular
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
-	
+
 	@Autowired
 	private IClienteService clienteService;
-	
+
 	@GetMapping("/clientes")
-	public List <Cliente> index() {
-		
+	public List<Cliente> index() {
+
 		return clienteService.findAll();
-		
+
 	}
-	
-	//Mostrar cliente por Id
+
+	// Mostrar cliente por Id
 	@GetMapping("/clientes/{id}")
-	public Cliente show (@PathVariable Long id) {
-		
-		return clienteService.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id) {
+
+		Cliente cliente = null;
+
+		Map<String, Object> response = new HashMap<>(); // clave, valor
+
+		try {
+
+			cliente = clienteService.findById(id);
+
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta en la BBDD");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+
+		}
+
+		if (cliente == null) {
+			response.put("mensaje", "El cliente ID: ".concat(id.toString().concat(" no existe en la BBDD")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
 	}
-	
-	//Crear un cliente
+
+	// Crear un cliente
 	@PostMapping("/clientes")
-	@ResponseStatus(HttpStatus.CREATED) //por defecto si no se pone nada es HttpStatus.OK, esto se pone para señalar que ha sido creado (no es obligatorio)
-	public Cliente create(@RequestBody Cliente cliente ) { //RequestBody pide un cliente en el cuerpo de la peticion frontend, que viene en formato json 
-		
+	@ResponseStatus(HttpStatus.CREATED) // por defecto si no se pone nada es HttpStatus.OK, esto se pone para señalar
+										// que ha sido creado (no es obligatorio)
+	public Cliente create(@RequestBody Cliente cliente) { // RequestBody pide un cliente en el cuerpo de la peticion
+															// frontend, que viene en formato json
+
 		return clienteService.save(cliente);
-		
+
 	}
-	
-	
-	//Editar cliente
+
+	// Editar cliente
 	@PutMapping("/clientes/{id}")
-	@ResponseStatus(HttpStatus.CREATED) 
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
-		
+
 		Cliente clienteActual = clienteService.findById(id);
-		clienteActual.setApellido(cliente.getApellido()); 
-		clienteActual.setNombre(cliente.getNombre()); 
-		clienteActual.setEmail(cliente.getEmail()); 
-		
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setEmail(cliente.getEmail());
+
 		return clienteService.save(clienteActual);
 	}
-	
-	
-	//Borrar cliente
+
+	// Borrar cliente
 	@DeleteMapping("/clientes/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT) //corresponde a estado 204
+	@ResponseStatus(HttpStatus.NO_CONTENT) // corresponde a estado 204
 	public void delete(@PathVariable Long id) {
 		clienteService.delete(id);
 	}

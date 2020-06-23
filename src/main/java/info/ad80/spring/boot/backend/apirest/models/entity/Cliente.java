a2152e2c0b931b97d61cc1953c3dc0cf56db347a
musiciancoder <andresguitarra1980@gmail.com>
2020-06-23 14:32:47 -0400

    90 añadiendo campo para fecha

--- src/main/java/info/ad80/spring/boot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/info/ad80/spring/boot/backend/apirest/models/entity/Cliente.java
@@ -8,12 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 
@@ -38,14 +38,12 @@ public class Cliente implements Serializable {
 	@Email(message="No es una dirección de correo correcta")
 	private String email;
 	
+	@NotNull(message="No puede ser vacío")
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE) //para transformar el Date de java a la fecha date de SQL
 	private Date createAt;
 	
-	@PrePersist
-	private void prePersist() {
-		createAt = (new Date()); // la fecha actual
-	}
+
 	
 	public Long getId() {
 		return id;

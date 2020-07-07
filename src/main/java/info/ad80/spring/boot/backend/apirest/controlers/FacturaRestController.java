05d9ba50e78cf484b25a4c0e6bf19905754915f9
musiciancoder <andresguitarra1980@gmail.com>
2020-07-07 18:57:15 -0400

    170

--- src/main/java/info/ad80/spring/boot/backend/apirest/controlers/FacturaRestController.java
+++ src/main/java/info/ad80/spring/boot/backend/apirest/controlers/FacturaRestController.java
@@ -3,6 +3,7 @@ package info.ad80.spring.boot.backend.apirest.controlers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,13 @@ public class FacturaRestController {
 	@ResponseStatus(HttpStatus.OK)
 	public Factura show (@PathVariable Long id) {
 		return clienteService.findFacturaByid(id);
+	}
+	
+	@DeleteMapping("/facturas/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	private void delete(@PathVariable Long id) {
+		clienteService.deleteFacturaById(id);
+
 	}
 
 }
